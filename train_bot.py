fbd3eccc140dd0ac4e08f64e8d6aa2a5784e0f08
Ophilia Fradarick <76613993+OphiliaFradarick@users.noreply.github.com>
2022-09-30 20:26:32 +0530

    Update train_bot.py

--- train_bot.py
+++ train_bot.py
@@ -24,9 +24,30 @@ def get_stem_words(words, ignore_words):
             stem_words.append(w)  
     return stem_words
 
-   # Add all words of patterns to list
-        
-   # Add all tags to the classes list
+# function for appending stem words
+def get_stem_words(words, ignore_words):
+    stem_words = []
+    for word in words:
+        if word not in ignore_words:
+            w = stemmer.stem(word.lower())
+            stem_words.append(w)  
+    return stem_words
+
+for intent in intents['intents']:
+    
+        # Add all words of patterns to list
+        for pattern in intent['patterns']:            
+            pattern_word = nltk.word_tokenize(pattern)            
+            words.extend(pattern_word)                      
+            word_tags_list.append((pattern_word, intent['tag']))
+        # Add all tags to the classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+            stem_words = get_stem_words(words, ignore_words)
+
+print(stem_words)
+print(word_tags_list[0]) 
+print(classes) 
          
 
 #Create word corpus for chatbot
